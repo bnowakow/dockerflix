19d37493b20649d5fe005c5e4885ba87fab21398
Bartek Nowakowski <bartek@bnowakowski.pl>
2015-05-10 09:38:15 +0200

    Change url to forked one

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 if [ $# -eq 0 ]
   then
-    docker build --no-cache=true -f dockerflix.us -t trick77/dockerflix .
+    docker build --no-cache=true -f dockerflix.us -t bnowakow/dockerflix .
   else
-    docker build --no-cache=true -f dockerflix.$1 -t trick77/dockerflix .
+    docker build --no-cache=true -f dockerflix.$1 -t bnowakow/dockerflix .
 fi
